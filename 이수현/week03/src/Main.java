3aab59d8b1c7e45889ac9db792c3339cc149bbda
sio <123susuhyun@gmail.com>
2026-03-31 13:25:33 +0900

    week3_이수현

--- 이수현/week03/src/Main.java
+++ 이수현/week03/src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static class Book {
+        String title;
+        int price;
+        Book(String title, int price) {
+            this.title =title;
+            this.price = price;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        }
+
+        void displayInfo() {
+            System.out.println("도서명: " + title + " | 가격: " + price + "원");
+        }
+
+        void displayInfo(String user) {
+            System.out.println("[" + user + "님의 조회 결과] 도서명: " + title);
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        try {
+            Book library[] = new Book[3];
+
+            library[0] = new Book("사자의 정석", 30000);
+            library[1] = new Book("멋사 입문", 22000);
+            library[2] = new Book("멋대 가이드", 28000);
+
+            System.out.println("=== 전체 도서 목록 ===");
+            for (int i = 0; i < library.length; i++) {
+                library[i].displayInfo();
+            }
+
+            System.out.println("\n=== 특정 사용자 맞춤 출력 (오버로딩) ===");
+            library[0].displayInfo("멋사");
+
+            System.out.println("\n=== 예외 발생 테스트 ===");
+            System.out.println(library[5].title);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("경고: 배열의 크기를 벗어난 접근입니다. (Index Error)");
+        } catch (Exception e) {
+            System.err.println("경고: 알 수 없는 오류가 발생했습니다.");
+        } finally {
+            System.out.println("\n[시스템] 리소스 정리 후 프로그램을 종료합니다.");
         }
     }
 }
